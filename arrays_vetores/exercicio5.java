3140d872c02aad8a9c0a3d9cbea200ac0df9ac12
Gabrielle Soares <gabrielle.sote@gmail.com>
2024-03-27 22:35:54 -0300

    oi

--- arrays_vetores/exercicio5.java
+++ arrays_vetores/exercicio5.java
@@ -1,4 +1,35 @@
 package arrays_vetores;
 
+import java.util.Scanner;
+
 public class exercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 6;
+        int qntPar = 0;
+
+        int vetor[] = new int[n];
+
+        for(int i = 0; i < n ; i++){
+            System.out.println("Digite um número: ");
+            vetor[i] = sc.nextInt();
+
+        }
+
+        System.out.println("Numero pares: ");
+        for(int i = 0; i < vetor.length;i++){
+            if(vetor[i] % 2 == 0){
+
+                System.out.println(vetor[i]);
+                qntPar +=1;
+            }
+        }
+
+        System.out.println("Quantidade pares: "+ qntPar);
+
+
+        sc.close();
+
+    }
 }
---
+++ arrays_vetores/exercicio6.java
@@ -0,0 +1,18 @@
+package arrays_vetores;
+
+import java.util.Scanner;
+
+public class exercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 6;
+
+        int vetor[] = new int[n];
+        int vetor2[] = new int[n];
+
+        int soma[];
+
+
+    }
+}
